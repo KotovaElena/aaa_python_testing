abe6d288ac2deed09e3f5c4088c5fdd974edbadc
KotovaElena <kotovalena2000@gmail.com>
2022-11-16 22:14:29 +0300

    Changed Issue-05

--- Issue-05/test_what_is_year_now.py
+++ Issue-05/test_what_is_year_now.py
@@ -1,11 +1,13 @@
 from unittest.mock import patch
 from what_is_year_now import what_is_year_now
 import pytest
+from io import StringIO
 
 
 def test_dmy():
     """Тест для даты формата 'DD.MM.YYYY'."""
-    with patch('urllib.request.urlopen'), patch('json.load', return_value={"currentDateTime": "15.11.2019"}):
+    expected_resp = '{"currentDateTime": "15.11.2019"}'
+    with patch('urllib.request.urlopen', return_value=StringIO(expected_resp)):
         year = what_is_year_now()
     expected_year = 2019
     assert year == expected_year
@@ -13,7 +15,8 @@ def test_dmy():
 
 def test_ymd():
     """Тест для даты формата 'YYYY-MM-DD'."""
-    with patch('urllib.request.urlopen'), patch('json.load', return_value={"currentDateTime": "2019-11-15"}):
+    expected_resp = '{"currentDateTime": "2019-11-15"}'
+    with patch('urllib.request.urlopen', return_value=StringIO(expected_resp)):
         year = what_is_year_now()
     expected_year = 2019
     assert year == expected_year
@@ -21,6 +24,7 @@ def test_ymd():
 
 def test_wrong_date_format():
     """Тест для даты в неправильном формате."""
-    with patch('urllib.request.urlopen'), patch('json.load', return_value={"currentDateTime": "2019/11/15"}):
+    expected_resp = '{"currentDateTime": "2019/11/15"}'
+    with patch('urllib.request.urlopen', return_value=StringIO(expected_resp)):
         with pytest.raises(ValueError):
             what_is_year_now()
